7a1ac98201b1ab05fa646670dcecf70366e68d8e
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-05 04:39:29 +0300

    Update tools.py

--- Kazu/fns/tools.py
+++ Kazu/fns/tools.py
@@ -71,36 +71,8 @@ async def get_ofox(codename):
     return device, releases
 
 
-# ~~~~~~~~~~~~~~~Async Searcher~~~~~~~~~~~~~~~
-# @buddhhu
-
-
-async def async_searcher(
-    url: str,
-    post: bool = None,
-    headers: dict = None,
-    params: dict = None,
-    json: dict = None,
-    data: dict = None,
-    ssl=None,
-    re_json: bool = False,
-    re_content: bool = False,
-    real: bool = False,
-):
-    async with aiohttp.ClientSession(headers=headers) as client:
-        if post:
-            data = await client.post(url, json=json, data=data, ssl=ssl)
-        else:
-            data = await client.get(url, params=params, ssl=ssl)
-        if re_json:
-            return await data.json()
-        if re_content:
-            return await data.read()
-        return data if real else await data.text()
-
 
 # ~~~~~~~~~~~~~~~JSON Parser~~~~~~~~~~~~~~~
-# @buddhhu
 
 
 def _unquote_text(text):
